5c33a09231a4d60f3532856152384b5782122a26
Eduard Urbach <e.urbach@gmail.com>
2016-08-28 22:28:51 +0900

    Implemented basics

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-module.exports = app => console.log('Authorization module started')
+let passport = require('passport')
+
+module.exports = app => {
+	if(!app.db)
+		throw new Error('The auth plugin needs to be loaded AFTER the database plugin')
+	
+	let db = app.db
+	
+	// Serialize
+	// This means we're reducing the user data to a single hash by which the user can be identified.
+	passport.serializeUser(function(request, user, done) {
+		db.set('Users', user.id, user)
+		.catch(console.error)
+		.finally(() => done(null, user.id))
+	})
+	
+	// Deserialize
+	// This means a web page is requesting full user data by some kind of hash.
+	passport.deserializeUser(function(id, done) {
+		db.get('Users', id)
+		.then(user => done(undefined, user))
+		.catch(error => done(undefined, false))
+	})
+	
+	// Middleware
+	app.use(
+		require('./lib/session')(app),
+		passport.initialize(),
+		passport.session()
+	)
+	
+	// Logout
+	app.get('/logout', function(request, response) {
+		request.logout()
+		
+		Promise.delay(100).then(() => {
+			response.redirect('/')
+		})
+	})
+}
