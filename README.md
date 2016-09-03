f3ca4a29f61807747f43f6a0bebe39931ab73d6f
Eduard Urbach <e.urbach@gmail.com>
2016-09-03 11:03:34 +0900

    Implemented basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # aero-auth
-Allow users to login.
+Allow users to login. Requires additional plugins for each login provider.
+
+## Installation
+Add `aero-auth` to `dependencies` in your `package.json`:
+
+```json
+"dependencies": {
+	"aero-auth": "*"
+}
+```
+
+Make sure you added a database plugin like [aerospike](https://github.com/aerojs/aero-aerospike) before `aero-auth` is loaded.
+
+### Session secret
+Your `security/api-keys.json` should include a session secret if you don't want sessions to be lost on server restart:
+
+```json
+{
+	"session": {
+		"secret": "11111111111111111111111111111111"
+	}
+}
+```
+
+## Database
+This plugin will populate the `Users` table and assume that `id` is defined for users. It will also use the `Sessions` table to store session data.
+
+## Logout
+This plugin will add the route `/logout` which destroys the session and redirects back to the frontpage.
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+let session = require('./lib/session')
 let passport = require('passport')
 
 module.exports = app => {
 	if(!app.db)
 		throw new Error('The auth plugin needs to be loaded AFTER the database plugin')
-	
+
+	if(!app.api.session || !app.api.session.secret)
+		console.warn('Warning: app.api.session.secret is not defined, you should add session.secret to security/api-keys.json (falling back to temporary sessions)')
+
 	let db = app.db
-	
+
 	// Serialize
 	// This means we're reducing the user data to a single hash by which the user can be identified.
 	passport.serializeUser(function(request, user, done) {
@@ -13,7 +17,7 @@ module.exports = app => {
 		.catch(console.error)
 		.finally(() => done(null, user.id))
 	})
-	
+
 	// Deserialize
 	// This means a web page is requesting full user data by some kind of hash.
 	passport.deserializeUser(function(request, id, done) {
@@ -21,18 +25,18 @@ module.exports = app => {
 		.then(user => done(undefined, user))
 		.catch(error => done(undefined, false))
 	})
-	
+
 	// Middleware
 	app.use(
-		require('./lib/session')(app),
+		session(app),
 		passport.initialize(),
 		passport.session()
 	)
-	
+
 	// Logout
 	app.get('/logout', function(request, response) {
 		request.logout()
-		
+
 		Promise.delay(100).then(() => {
 			response.redirect('/')
 		})
